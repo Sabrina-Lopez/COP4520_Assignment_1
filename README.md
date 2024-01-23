974fc23a186a0c2d7cc1cf84575f579c178b2b7d
Sabrina-Lopez <sabrinameganlopez015@gmail.com>
2024-01-23 18:43:22 -0500

    removed unnecessary files, added commends, modified README

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# COP4520_Assignment_1
+# How to run assignment
+- Open to VSC
+- Click on the primesMultithread.java file 
+- Right click on the code of primesMultithread.java file
+- Click "Run Code"
+
+# Results in chronological order
+- Time taken for the threads to run
+- Sum of all the prime values
+- The number of prime values
+- The top 10 prime values
+
  
--- primesMultithread.java
+++ primesMultithread.java
@@ -1,4 +1,3 @@
-package COP4520_Assignment_1;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -9,40 +8,67 @@ public class primesMultithread {
     private static final int NUM_THREADS = 8;
 
     public static void main(String[] args) {
+        // Declare the maximum value to find primes up to from 0
         int limit = (int) Math.pow(10, 8);
 
+        // Declare threads and the array of classes with the number of desired threads
         Thread[] threads = new Thread[NUM_THREADS];
         PrimeInfo[] primeInfoTasks = new PrimeInfo[NUM_THREADS];
 
+        // Evenly separate the number of values to check if they're prime among the threads
         int segmentSize = limit / NUM_THREADS;
+
+        // Start value is 2 as 0 and 1 are not prime values
         int start = 2;
+
+        // Current end value is the end of the values that will be processed by the first thread
         int end = segmentSize;
 
+        // Get the start time right before the threads start processing
         long startTime = System.currentTimeMillis();
 
+        // Declare the method to create and write to an external file
         try (FileWriter writer = new FileWriter("primes.txt")) {
+            // For each thread
             for (int i = 0; i < NUM_THREADS; i++) {
+
+                // If the program has eached the last thread
                 if (i == NUM_THREADS - 1) {
+
+                    // The end is the limit
                     end = limit;
                 }
 
+                // Initialize the current class in the array to contain the information regarding the current set of values, give the
+                    // class to its respective current thread, and start the thread
                 primeInfoTasks[i] = new PrimeInfo(start, end);
                 threads[i] = new Thread(primeInfoTasks[i]);
                 threads[i].start();
 
+                // Move the start and end to the next set of values to process
                 start = end + 1;
                 end += segmentSize;
             }
 
+            // Initialize variables to contain the sum of the prime values, the total number of prime values, and the top 10 prime values
+                // from 0 to the limit
             long sum = 0;
             int totalPrimeCount = 0;
             List<Integer> topPrimes = new ArrayList<>();
 
+            // For each thread
             for (int i = 0; i < NUM_THREADS; i++) {
                 try {
+
+                    // Get the current thread
                     threads[i].join();
+
+                    // Get the sum of the current thread's class's getSum function
                     sum += primeInfoTasks[i].getSum();
+
+                    // Get the totle number of primes of the current thread's class's getPrimeCount function
                     totalPrimeCount += primeInfoTasks[i].getPrimeCount();
+                    // Get the top 10 primes of the current thread's class's getTopPrimes function
                     topPrimes.addAll(primeInfoTasks[i].getTopPrimes());
                 } catch (InterruptedException e) {
                     e.printStackTrace();
@@ -53,9 +79,12 @@ public class primesMultithread {
             Collections.sort(topPrimes, Collections.reverseOrder());
             topPrimes = topPrimes.subList(0, Math.min(topPrimes.size(), 10));
 
+            // Get the end time right after the threads finish processing
             long endTime = System.currentTimeMillis();
 
             try {
+
+                // Output the thread processing time, sum of the primes, total number of prime values, and the top 10 primes to the external file
                 writer.write((endTime - startTime) + "\n");
                 writer.write(sum + "\n");
                 writer.write(totalPrimeCount + "\n");
@@ -71,6 +100,7 @@ public class primesMultithread {
         }
     }
 
+    // Create class to handle run() and other necessary functions to get the desired outputted information
     static class PrimeInfo implements Runnable {
         private int start;
         private int end;
@@ -84,18 +114,22 @@ public class primesMultithread {
             this.sum = 0;
         }
 
+        // Get the sum of the primes for the thread
         public long getSum() {
             return sum;
         }
 
+        // Get the total number of primes for the thread
         public int getPrimeCount() {
             return primeCount;
         }
 
+        // Get the top 10 prime values for the thread
         public List<Integer> getTopPrimes() {
             return topPrimes;
         }
 
+        // Function to determine if the current value is prime
         public boolean isPrime(int number) {
             if (number < 2) {
                 return false;
@@ -110,6 +144,7 @@ public class primesMultithread {
             return true;
         }
 
+        // Thread's function to get the necessary output information for the current segment of values between start and end
         @Override
         public void run() {
             for (int i = start; i <= end; i++) {
